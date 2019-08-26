304aadb676a698e82b5987b1b1d09db1be05ebbe
Brad Jenner <bradjenner@Brads-MacBook-Pro-2.local>
2019-08-26 00:58:28 -0700

    home updates

---
+++ scripts/repo.js
@@ -0,0 +1,22 @@
+var pubContainer = document.getElementById("repositories");
+var ourRequest = new XMLHttpRequest();
+
+ourRequest.open('GET', 'https://api.github.com/users/bnjenner/repos');
+ourRequest.onload = function() {
+  var ourData = JSON.parse(ourRequest.responseText);
+  renderHTML(ourData);
+
+};
+ourRequest.send( );
+
+function renderHTML(data) {
+  
+  var htmlString = ""; 
+
+  for (i = 0; i < data.length; i++) {
+    if (data[i].name != "Publications" && data[i].name != "bnjenner.github.io") { 
+      htmlString += "<p><a href=\"" + data[i].html_url + "\"  target=\"_blank\">" + data[i].name + "</a></p>";
+    }
+  }
+  pubContainer.insertAdjacentHTML('beforeend', htmlString);
+}
